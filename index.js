c808eccc6af6a559730ff9db9d4b349a438305ea
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-08-24 16:32:10 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+let events = "surprise";
+function writeCards(){
+    let messages = [];
+    // map updated and unique messages to the messages array
+    names.map(name=> messages[names.indexOf(name)] = `Thank you, ${names[names.indexOf(name)]}, for the wonderful ${events} gift!`)
+    return messages;
+}
+function countDown(number){
+    number = 10;
+    while(number >= 0){
+        console.log(number)
+        number --;
+    }
+}
+
+// console.log(countDown())
